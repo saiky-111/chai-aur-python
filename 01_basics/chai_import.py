24502ea3a567192df42787c4f3480e92a21c907c
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-06-12 16:20:26 +0530

    new commit

---
+++ 01_basics/chai_import.py
@@ -0,0 +1,15 @@
+ #in this file , we will the run(bring) the method writen in the file hello_world.py and print here.
+
+from hello_world import chai  # means from hello_woeld.py(file name) file import the method written in the other file.
+                               #now you can run the chai method in the same way here also , as you ran in the hello_world.py file
+                              # without again defining here again.
+
+chai("ginger tea")
+
+#Output:
+# hello world
+# lemon tea
+# 4
+# ginger tea
+
+#also when this file a new folder is created i.e. __pycache__ and a file name hello_world.cpython-312.pyc is created.
--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+def chai(n): #basically defined a function, name is chai, it takes input n, n can be anything, and it prints n.
+    print(n)
+    #def is a defination ,in place of chai you can give any name
+    #in place of n you can write anything, then colon , enter
+    #Cursor will become indent, i.e will give indentation
+
+
+chai("lemon tea")
+chai(4) # now anyhting you write inside it, will get printed.
